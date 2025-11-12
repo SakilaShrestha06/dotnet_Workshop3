78488ec70684cf84b47d953d2d2f81d2b2f1d6e1
SakilaShrestha06 <shresthasakila0663@gmail.com>
2025-11-12 20:57:06 +0545

    second commit

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,9 +1,28 @@
-﻿namespace Task3;
+﻿using System;
 
-class Program
+namespace Task3;
+internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.Write("Enter your age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        if (age < 13)
+        {
+            Console.WriteLine("Child");
+        }
+        else if (age >= 13 && age <= 19)
+        {
+            Console.WriteLine("Teenager");
+        }
+        else if (age >= 20 && age <= 59)
+        {
+            Console.WriteLine("Adult");
+        }
+        else
+        {
+            Console.WriteLine("Senior Citizen");
+        }
     }
 }
